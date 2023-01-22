4f4c0862dc4aae2fe8aaf3de4b4a769562a143d0
Michael Nahkies <michael@nahkies.co.nz>
2023-01-22 10:02:44 +0000

    chore: upgrade angular test to use latest angular (v15)

--- integration-tests/typescript-angular/src/app/app.module.ts
+++ integration-tests/typescript-angular/src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ApiModule } from "./api.github.com.json/api.module"
+import {ApiModule} from "./api.github.com.json/api.module";
 
 @NgModule({
   declarations: [
