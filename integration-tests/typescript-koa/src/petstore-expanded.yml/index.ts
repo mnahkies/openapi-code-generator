97a66e2bf61c002240f8a068f6892961ead378ea
Michael Nahkies <michael@nahkies.co.nz>
2023-02-05 21:46:26 +0000

    add example implementation

---
+++ integration-tests/typescript-koa/src/petstore-expanded.yml/index.ts
@@ -0,0 +1,42 @@
+import { bootstrap, FindPetById } from "./generated"
+
+const notImplemented = async () => {
+  return { status: 501, body: { error: 'not implemented' } }
+}
+
+const findPetById: FindPetById = async (params, ctx) => {
+  switch (params.params.id) {
+    case 1:
+      return {
+        status: 200,
+        body: {
+          name: "Jake",
+          breed: "border-collie",
+        },
+      }
+
+    case 2:
+      return {
+        status: 200,
+        body: {
+          name: "Lacy",
+          breed: "border-collie",
+        },
+      }
+
+    default:
+      return {
+        status: 404,
+        body: {
+          error: 'not found',
+        },
+      }
+  }
+}
+
+bootstrap({
+  findPets: notImplemented,
+  findPetById,
+  addPet: notImplemented,
+  deletePet: notImplemented,
+}, { port: 3000 })
