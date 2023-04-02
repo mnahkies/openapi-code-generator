d028dbf4eeb9439914d931d597040f2684d987f9
Michael Nahkies <michael@nahkies.co.nz>
2023-04-02 12:27:08 +0100

    chore: re-generate integration tests

--- integration-tests/typescript-koa/src/petstore-expanded.yml/generated.ts
+++ integration-tests/typescript-koa/src/petstore-expanded.yml/generated.ts
@@ -13,18 +13,12 @@ import {
   ServerConfig,
   startServer,
 } from "@nahkies/typescript-koa-runtime/server"
-import { parseRequestInput } from "@nahkies/typescript-koa-runtime/zod"
-import { Context, Next } from "koa"
+import { Params, parseRequestInput } from "@nahkies/typescript-koa-runtime/zod"
+import { Context } from "koa"
 import { z } from "zod"
 
 //region safe-edit-region-header
 //endregion safe-edit-region-header
-
-type Params<Params, Query, Body> = { params: Params; query: Query; body: Body }
-
-interface ValidatedCtx<Params, Query, Body> extends Context {
-  state: { params: Params; query: Query; body: Body }
-}
 
 export type FindPets = (
   params: Params<void, t_FindPetsQuerySchema, void>,
@@ -65,91 +59,66 @@ export function bootstrap(
     limit: z.coerce.number().optional(),
   })
 
-  router.get(
-    "findPets",
-    "/pets",
-    async (
-      ctx: ValidatedCtx<void, t_FindPetsQuerySchema, void>,
-      next: Next
-    ) => {
-      const input = {
-        params: undefined,
-        query: parseRequestInput(findPetsQuerySchema, ctx.query),
-        body: undefined,
-      }
-
-      const { status, body } = await implementation.findPets(input, ctx)
-      ctx.status = status
-      ctx.body = body
-      return next()
+  router.get("findPets", "/pets", async (ctx, next) => {
+    const input = {
+      params: undefined,
+      query: parseRequestInput(findPetsQuerySchema, ctx.query),
+      body: undefined,
     }
-  )
+
+    const { status, body } = await implementation.findPets(input, ctx)
+    ctx.status = status
+    ctx.body = body
+    return next()
+  })
 
   const addPetBodySchema = z.object({
     name: z.coerce.string(),
     tag: z.coerce.string().optional(),
   })
 
-  router.post(
-    "addPet",
-    "/pets",
-    async (ctx: ValidatedCtx<void, void, t_AddPetBodySchema>, next: Next) => {
-      const input = {
-        params: undefined,
-        query: undefined,
-        body: parseRequestInput(addPetBodySchema, ctx.body),
-      }
-
-      const { status, body } = await implementation.addPet(input, ctx)
-      ctx.status = status
-      ctx.body = body
-      return next()
+  router.post("addPet", "/pets", async (ctx, next) => {
+    const input = {
+      params: undefined,
+      query: undefined,
+      body: parseRequestInput(addPetBodySchema, ctx.body),
     }
-  )
+
+    const { status, body } = await implementation.addPet(input, ctx)
+    ctx.status = status
+    ctx.body = body
+    return next()
+  })
 
   const findPetByIdParamSchema = z.object({ id: z.coerce.number() })
 
-  router.get(
-    "findPetById",
-    "/pets/:id",
-    async (
-      ctx: ValidatedCtx<t_FindPetByIdParamSchema, void, void>,
-      next: Next
-    ) => {
-      const input = {
-        params: parseRequestInput(findPetByIdParamSchema, ctx.params),
-        query: undefined,
-        body: undefined,
-      }
-
-      const { status, body } = await implementation.findPetById(input, ctx)
-      ctx.status = status
-      ctx.body = body
-      return next()
+  router.get("findPetById", "/pets/:id", async (ctx, next) => {
+    const input = {
+      params: parseRequestInput(findPetByIdParamSchema, ctx.params),
+      query: undefined,
+      body: undefined,
     }
-  )
+
+    const { status, body } = await implementation.findPetById(input, ctx)
+    ctx.status = status
+    ctx.body = body
+    return next()
+  })
 
   const deletePetParamSchema = z.object({ id: z.coerce.number() })
 
-  router.delete(
-    "deletePet",
-    "/pets/:id",
-    async (
-      ctx: ValidatedCtx<t_DeletePetParamSchema, void, void>,
-      next: Next
-    ) => {
-      const input = {
-        params: parseRequestInput(deletePetParamSchema, ctx.params),
-        query: undefined,
-        body: undefined,
-      }
-
-      const { status, body } = await implementation.deletePet(input, ctx)
-      ctx.status = status
-      ctx.body = body
-      return next()
+  router.delete("deletePet", "/pets/:id", async (ctx, next) => {
+    const input = {
+      params: parseRequestInput(deletePetParamSchema, ctx.params),
+      query: undefined,
+      body: undefined,
     }
-  )
+
+    const { status, body } = await implementation.deletePet(input, ctx)
+    ctx.status = status
+    ctx.body = body
+    return next()
+  })
 
   return startServer({
     middleware: [],
