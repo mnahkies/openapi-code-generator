915456fa281181652b831d036c65a0387c6cf58b
Michael Nahkies <michael@nahkies.co.nz>
2023-04-10 19:23:53 +0100

    fix: improve validation to switch between 3.1.0 / 3.0.0 (#34)

--- packages/openapi-code-generator/src/core/openapi-validator.ts
+++ packages/openapi-code-generator/src/core/openapi-validator.ts
@@ -1,28 +1,50 @@
 import Ajv2020, {ValidateFunction} from "ajv/dist/2020"
 import addFormats from "ajv-formats"
 import {promptContinue} from "./cli-utils"
-import openapi3Specification = require("./openapi-3-specification.json")
+import openapi3_1_specification = require("./schemas/openapi-3.1-specification.json")
+import openapi3_0_specification = require("./schemas/openapi-3.0-specification.json")
 import {logger} from "./logger"
+import AjvDraft04 from "ajv-draft-04"
 
 export class OpenapiValidator {
 
-  private readonly validationFunction: ValidateFunction
+  private readonly validate3_1: ValidateFunction
+  private readonly validate3_0: ValidateFunction
 
   private constructor() {
-    const ajv = new Ajv2020({strict: false})
-    addFormats(ajv)
-    ajv.addFormat("media-range", true)
-    this.validationFunction = ajv.compile(openapi3Specification)
+    const ajv2020 = new Ajv2020({strict: false})
+    addFormats(ajv2020)
+    ajv2020.addFormat("media-range", true)
+
+    this.validate3_1 = ajv2020.compile(openapi3_1_specification)
+
+    const ajv4 = new AjvDraft04({strict: false})
+    addFormats(ajv4)
+
+    this.validate3_0 = ajv4.compile(openapi3_0_specification)
+  }
+
+  private validationFunction(version: string) {
+    if (version.startsWith("3.0")) {
+      return this.validate3_0
+    } else if (version.startsWith("3.1")) {
+      return this.validate3_1
+    }
+
+    throw new Error(`unsupported openapi version '${version}'`)
   }
 
   async validate(filename: string, schema: unknown): Promise<void> {
-    const isValid = this.validationFunction(schema)
+    const version = (schema && typeof schema === "object" && Reflect.get(schema, "openapi")) || "unknown"
+    const validate = this.validationFunction(version)
+
+    const isValid = validate(schema)
 
     if (!isValid) {
       logger.warn(`Found errors validating '${filename}'.`)
       logger.warn("Note errors may cascade, and should be investigated top to bottom. Errors:\n")
 
-      this.validationFunction.errors?.forEach(err => {
+      validate.errors?.forEach(err => {
         logger.warn(`-> ${err.message} at path '${err.instancePath}'`, err.params)
       })
 
--- scripts/refresh-data.sh
+++ scripts/refresh-data.sh
@@ -3,7 +3,8 @@
 set -e
 
 # Openapi v3.1 schema definitions
-curl https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json -o ./packages/openapi-code-generator/src/core/openapi-3-specification.json
+curl https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json -o ./packages/openapi-code-generator/src/core/schemas/openapi-3.1-specification.json
+curl https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.0/schema.json -o ./packages/openapi-code-generator/src/core/schemas/openapi-3.0-specification.json
 
 # Example API Definitions
 curl -L https://raw.githubusercontent.com/github/rest-api-description/main/descriptions/api.github.com/api.github.com.yaml -o ./integration-tests-definitions/api.github.com.yaml
