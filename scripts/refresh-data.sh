fd5828a8ff599ed72abcb4e40561c39e52d49868
Michael Nahkies <michael@nahkies.co.nz>
2023-04-22 13:33:44 +0100

    refactor: consistently use yaml instead of yml (#47)

--- scripts/refresh-data.sh
+++ scripts/refresh-data.sh
@@ -8,4 +8,4 @@ curl https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3
 
 # Example API Definitions
 curl -L https://raw.githubusercontent.com/github/rest-api-description/main/descriptions/api.github.com/api.github.com.yaml -o ./integration-tests-definitions/api.github.com.yaml
-curl -L https://raw.githubusercontent.com/OAI/OpenAPI-Specification/master/examples/v3.0/petstore-expanded.yaml -o ./integration-tests-definitions/petstore-expanded.yml
+curl -L https://raw.githubusercontent.com/OAI/OpenAPI-Specification/master/examples/v3.0/petstore-expanded.yaml -o ./integration-tests-definitions/petstore-expanded.yaml
