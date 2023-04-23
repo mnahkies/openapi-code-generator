dbcec86d19932a1ed3180934bebea811c240aff7
Michael Nahkies <michael@nahkies.co.nz>
2023-04-23 07:48:52 +0100

    fix: switch to qs and add tests for query string (#56)
    - switch to qs package as this advertises support for arrays/objects
    more so than the built-in node:querystring library
    
    - add tests for query string serialization
    
    I'm not 100% sure that this is actually what we want, or at least what
    we always want. It could well be that we need to provide configuration
    to control how arrays/objects are handled for serialization, as I
    suspect different API's will have different opinions on this.
    
    Currently it's also ignoring the serialization hint parameters like
    style/explode - just adding a todo for now.

---
+++ packages/typescript-fetch-runtime/src/main.spec.ts
@@ -0,0 +1,58 @@
+import { describe, expect } from "@jest/globals"
+import { AbstractFetchClient, QueryParams } from "./main"
+
+class ConcreteFetchClient extends AbstractFetchClient {
+  constructor() {
+    super({ basePath: "", defaultHeaders: {} })
+  }
+
+  query(params: QueryParams) {
+    return this._query(params)
+  }
+}
+
+describe("main", () => {
+  const client = new ConcreteFetchClient()
+
+  describe("_query", () => {
+    it("returns an empty string when all params are undefined", () => {
+      expect(client.query({ foo: undefined, bar: undefined })).toBe("")
+    })
+
+    it("returns the defined params in a simple case", () => {
+      expect(
+        client.query({ foo: undefined, bar: "baz", foobar: "value" })
+      ).toBe("?bar=baz&foobar=value")
+    })
+
+    it("repeats array params", () => {
+      expect(client.query({ foo: ["bar", "baz"] })).toBe("?foo=bar&foo=baz")
+    })
+
+    it("handles objects", () => {
+      expect(client.query({ foo: { bar: "baz" } })).toBe(
+        `?${encodeURIComponent("foo[bar]")}=baz`
+      )
+    })
+
+    it("handles arrays of objects with multiple properties", () => {
+      expect(
+        client.query({
+          foo: [
+            { prop1: "one", prop2: "two" },
+            { prop1: "foo", prop2: "bar" },
+          ],
+          limit: 10,
+          undefined: undefined,
+          includeSomething: false,
+        })
+      ).toBe(
+        `?${encodeURIComponent("foo[prop1]")}=one&${encodeURIComponent(
+          "foo[prop2]"
+        )}=two&${encodeURIComponent("foo[prop1]")}=foo&${encodeURIComponent(
+          "foo[prop2]"
+        )}=bar&limit=10&includeSomething=false`
+      )
+    })
+  })
+})
