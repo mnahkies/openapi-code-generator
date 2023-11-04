129678330f22bdeed98f208b066aeac241f39c17
Michael Nahkies <michael@nahkies.co.nz>
2023-11-04 10:44:43 +0000

    chore: yarn 4, @swc/jest (#89)
    - upgrade to yarn 4
    - replace `ts-node` / `ts-jest` with `@swc/jest` to workaround lack of
    support for extends with multiple files
    https://github.com/TypeStrong/ts-node/issues/2000
    - fix caching of dependencies in ci
    - remove nodemon
    - update dependencies

---
+++ jest.base.js
@@ -0,0 +1,13 @@
+/**
+ * @type { import('@jest/types').Config.ProjectConfig }
+ */
+const config = {
+  testEnvironment: "node",
+  transform: {
+    "^.+\\.(t|j)sx?$": "@swc/jest",
+  },
+  resetMocks: true,
+  testMatch: ["**/*.spec.ts"]
+}
+
+module.exports = config
---
+++ jest.config.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const inspector = require("inspector")
+
+/**
+ * @type { import('@jest/types').Config.GlobalConfig }
+ */
+const config = {
+  projects: ["packages/*/jest.config.js"],
+  testTimeout: inspector.url() ? 5 * 60 * 1000 : 5 * 1000,
+}
+
+module.exports = config
