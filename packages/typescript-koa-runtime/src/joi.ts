0b11e4051977a83195f62c3ccefe69a3809e9f9a
Michael Nahkies <michael@nahkies.co.nz>
2023-11-04 11:28:46 +0000

    feat: throw specialized errors indicating point of failure

--- packages/typescript-koa-runtime/src/joi.ts
+++ packages/typescript-koa-runtime/src/joi.ts
@@ -1,47 +1,62 @@
-import type {Schema as JoiSchema} from "@hapi/joi"
+/**
+ * @prettier
+ */
 
-export type Params<Params, Query, Body> = { params: Params, query: Query, body: Body }
+import type {Schema as JoiSchema} from "@hapi/joi"
+import {KoaRuntimeError, RequestInputType} from "./errors"
+
+export type Params<Params, Query, Body> = {
+  params: Params
+  query: Query
+  body: Body
+}
 
 // Note: joi types don't appear to have an equivalent of z.infer,
 //       hence any seems about as good as we can do here.
 export function parseRequestInput<Schema extends JoiSchema>(
   schema: Schema,
-  input: unknown
-): any;
+  input: unknown,
+  type: RequestInputType,
+): any
 export function parseRequestInput(
   schema: undefined,
-  input: unknown
-): undefined;
+  input: unknown,
+  type: RequestInputType,
+): undefined
 export function parseRequestInput<Schema extends JoiSchema>(
   schema: Schema | undefined,
   input: unknown,
+  type: RequestInputType,
 ): any {
+  try {
+    if (!schema) {
+      return undefined
+    }
 
-  if (!schema) {
-    return undefined
+    const result = schema.validate(input, {stripUnknown: true})
+
+    if (result.error) {
+      throw result.error
+    }
+
+    return result.value
+  } catch (err) {
+    throw KoaRuntimeError.RequestError(err, type)
   }
-
-  const result = schema.validate(input, {stripUnknown: true})
-
-  if (result.error) {
-    throw result.error
-  }
-
-  return result.value
 }
 
-export function responseValidationFactory(possibleResponses: [string, JoiSchema][], defaultResponse?: JoiSchema) {
-
+export function responseValidationFactory(
+  possibleResponses: [string, JoiSchema][],
+  defaultResponse?: JoiSchema,
+) {
   // Exploit the natural ordering matching the desired specificity of eg: 404 vs 4xx
-  possibleResponses.sort((x, y) => x[0] < y[0] ? -1 : 1)
+  possibleResponses.sort((x, y) => (x[0] < y[0] ? -1 : 1))
 
   return (status: number, value: unknown) => {
-
     for (const [match, schema] of possibleResponses) {
-
       const isMatch =
-        /^\d+$/.test(match) && String(status) === match ||
-        /^\d[xX]{2}$/.test(match) && String(status)[0] === match[0]
+        (/^\d+$/.test(match) && String(status) === match) ||
+        (/^\d[xX]{2}$/.test(match) && String(status)[0] === match[0])
 
       if (isMatch) {
         const result = schema.validate(value)
