01195ec3cac373d86aa2cad96379d95139ce0f76
Michael Nahkies <michael@nahkies.co.nz>
2023-11-04 10:17:03 +0000

    chore: yarn 4, replace ts-jest / ts-node with @swc/jest

---
+++ packages/typescript-fetch-runtime/jest.config.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const base = require( "../../jest.base")
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const {name: displayName} = require("./package.json")
+
+/**
+ * @type { import('@jest/types').Config.ProjectConfig }
+ */
+const config = {
+  ...base,
+  displayName
+}
+
+module.exports = config
