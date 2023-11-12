329a27b33bad62314e1af00cf3777764d6985da3
Michael Nahkies <michael@nahkies.co.nz>
2023-11-12 09:57:56 +0000

    fix: skip broken openapi 3.1 validation for now

--- scripts/refresh-data.sh
+++ scripts/refresh-data.sh
@@ -4,6 +4,7 @@ set -e
 
 # Openapi v3.1 schema definitions
 curl https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json -o ./packages/openapi-code-generator/src/core/schemas/openapi-3.1-specification.json
+curl https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema-base.json -o ./packages/openapi-code-generator/src/core/schemas/openapi-3.1-specification-base.json
 curl https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.0/schema.json -o ./packages/openapi-code-generator/src/core/schemas/openapi-3.0-specification.json
 
 # Example API Definitions
