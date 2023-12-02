436448c8d23e63e56a53650415e700a60ffe1f88
Michael Nahkies <michael@nahkies.co.nz>
2023-12-02 09:10:57 +0000

    fix: adjust after rebase

--- packages/openapi-code-generator/src/typescript/typescript-axios/typescript-axios.generator.ts
+++ packages/openapi-code-generator/src/typescript/typescript-axios/typescript-axios.generator.ts
@@ -1,27 +1,34 @@
 /**
  * @prettier
  */
-import {Input} from "../../core/input"
 import {emitGenerationResult} from "../common/output-utils"
 import {TypeBuilder} from "../common/type-builder"
 import {TypescriptAxiosClientBuilder} from "./typescript-axios-client-builder"
+import {ImportBuilder} from "../common/import-builder"
+import {schemaBuilderFactory} from "../common/schema-builders/schema-builder"
+import {OpenapiGeneratorConfig} from "../../templates.types"
 
-export async function generateTypescriptAxios({
-  dest,
-  input,
-}: {
-  dest: string
-  input: Input
-}): Promise<void> {
-  const models = TypeBuilder.fromInput("./models.ts", input)
-
+export async function generateTypescriptAxios(
+  config: OpenapiGeneratorConfig,
+): Promise<void> {
+  const input = config.input
+  const imports = new ImportBuilder()
+  const types = TypeBuilder.fromInput("./models.ts", input).withImports(imports)
+  const schemaBuilder = schemaBuilderFactory(
+    config.schemaBuilder,
+    input,
+    imports,
+  )
   const client = new TypescriptAxiosClientBuilder(
     "client.ts",
     "ApiClient",
-    models,
+    input,
+    imports,
+    types,
+    schemaBuilder,
   )
 
   input.allOperations().map((it) => client.add(it))
 
-  await emitGenerationResult(dest, [models, client])
+  await emitGenerationResult(config.dest, [types, client, schemaBuilder])
 }
