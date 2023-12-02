834766961c726fdc63a11f947a4b9089246a27d1
Michael Nahkies <michael@nahkies.co.nz>
2023-12-02 09:04:34 +0000

    feat: include schema builder in client generators

--- packages/openapi-code-generator/src/typescript/common/client-builder.ts
+++ packages/openapi-code-generator/src/typescript/common/client-builder.ts
@@ -1,34 +1,45 @@
+/**
+ * @prettier
+ */
 import {IROperation} from "../../core/openapi-types-normalized"
 import {ImportBuilder} from "./import-builder"
 import {TypeBuilder} from "./type-builder"
 import {ClientOperationBuilder} from "./client-operation-builder"
+import {Input} from "../../core/input"
+import {SchemaBuilder} from "./schema-builders/schema-builder"
 
 export abstract class TypescriptClientBuilder {
-  private readonly imports: ImportBuilder
-  private readonly models: TypeBuilder
   private readonly operations: string[] = []
 
   constructor(
     public readonly filename: string,
     public readonly name: string,
-    models: TypeBuilder,
+    protected readonly input: Input,
+    protected readonly imports: ImportBuilder,
+    protected readonly models: TypeBuilder,
+    protected readonly schemaBuilder: SchemaBuilder,
   ) {
-    this.imports = new ImportBuilder()
-    this.models = models.withImports(this.imports)
-    this.buildImports(this.imports)
+    this.buildImports(imports)
   }
 
   add(operation: IROperation): void {
-    const builder = new ClientOperationBuilder(operation, this.models)
+    const builder = new ClientOperationBuilder(
+      operation,
+      this.models,
+      this.schemaBuilder,
+    )
     const result = this.buildOperation(builder)
     this.operations.push(result)
   }
 
-  protected abstract buildImports(imports: ImportBuilder): void;
+  protected abstract buildImports(imports: ImportBuilder): void
 
-  protected abstract buildOperation(builder: ClientOperationBuilder): string;
+  protected abstract buildOperation(builder: ClientOperationBuilder): string
 
-  protected abstract buildClient(clientName: string, clientMethods: string[]): string;
+  protected abstract buildClient(
+    clientName: string,
+    clientMethods: string[],
+  ): string
 
   toString(): string {
     return `
--- packages/openapi-code-generator/src/typescript/typescript-angular/typescript-angular.generator.ts
+++ packages/openapi-code-generator/src/typescript/typescript-angular/typescript-angular.generator.ts
@@ -1,32 +1,45 @@
+/**
+ * @prettier
+ */
 import {TypeBuilder} from "../common/type-builder"
-import {Input} from "../../core/input"
 import {emitGenerationResult} from "../common/output-utils"
 import {AngularModuleBuilder} from "./angular-module-builder"
 import {AngularServiceBuilder} from "./angular-service-builder"
+import {OpenapiGeneratorConfig} from "../../templates.types"
+import {ImportBuilder} from "../common/import-builder"
+import {schemaBuilderFactory} from "../common/schema-builders/schema-builder"
 
-export async function generateTypescriptAngular({dest, input}: { dest: string, input: Input }): Promise<void> {
-  const models = TypeBuilder.fromInput("./models.ts", input)
+export async function generateTypescriptAngular(
+  config: OpenapiGeneratorConfig,
+): Promise<void> {
+  const input = config.input
+  const imports = new ImportBuilder()
+  const types = TypeBuilder.fromInput("./models.ts", input).withImports(imports)
+  const schemaBuilder = schemaBuilderFactory(
+    config.schemaBuilder,
+    input,
+    imports,
+  )
 
   const client = new AngularServiceBuilder(
     "client.service.ts",
     "ApiClient",
-    models,
+    input,
+    imports,
+    types,
+    schemaBuilder,
   )
 
-  input.allOperations()
-    .map(it => client.add(it))
+  input.allOperations().map((it) => client.add(it))
 
-  const module = new AngularModuleBuilder(
-    "api.module.ts",
-    "Api",
-  )
+  const module = new AngularModuleBuilder("api.module.ts", "Api")
 
-  module.provides("./" + client.filename)
-    .add(client.name)
+  module.provides("./" + client.filename).add(client.name)
 
-  await emitGenerationResult(dest, [
-    models,
+  await emitGenerationResult(config.dest, [
+    types,
     client,
     module,
+    schemaBuilder,
   ])
 }
