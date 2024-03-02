5e533ef0e2b6901437665ab1af4d3087dad62835
Michael Nahkies <michael@nahkies.co.nz>
2024-03-02 16:19:39 +0000

    fix: silence logs except on fail

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,7 @@ const inspector = require("inspector")
 const config = {
   projects: ["packages/*/jest.config.js"],
   testTimeout: inspector.url() ? 5 * 60 * 1000 : 5 * 1000,
+  reporters: ["./jest/reporter.js"],
 }
 
 module.exports = config
