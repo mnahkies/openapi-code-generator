2f4dd69a9c1b54112890d9a9c3b26d8ef79febf2
Michael Nahkies <michael@nahkies.co.nz>
2024-04-08 08:55:42 +0100

    feat: add typescript-nextjs

--- integration-tests/typescript-nextjs/src/app/page.tsx
+++ integration-tests/typescript-nextjs/src/app/page.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Image from "next/image"
+import styles from "./page.module.css"
 
 export default function Home() {
   return (
@@ -91,5 +91,5 @@ export default function Home() {
         </a>
       </div>
     </main>
-  );
+  )
 }
