b60af6281e792f2cf84aaf3c12607864581cd587
Michael Nahkies <michael@nahkies.co.nz>
2024-04-13 10:36:13 +0100

    feat: support loading input from uri
    initial support for loading input schemas from urls.
    
    support for following relative links to other urls probably doesn't work
    yet.
    
    relates #43

--- packages/openapi-code-generator/src/core/file-loader.ts
+++ packages/openapi-code-generator/src/core/file-loader.ts
@@ -22,36 +22,48 @@ async function loadLocalFile(file: string): Promise<[string, any]> {
   file = path.resolve(file)
   const raw = fs.readFileSync(file, {encoding: "utf-8"})
 
-  let result: any | undefined
+  return [file, await parseFile(raw, file)]
+}
 
-  if (file.endsWith(".json")) {
+async function loadRemoteFile(uri: string): Promise<[string, any]> {
+  const res = await fetch(uri)
+
+  if (!res.ok) {
+    throw new Error(`failed to fetch remote file '${uri}'`)
+  }
+
+  const raw = await res.text()
+
+  return [uri, await parseFile(raw, uri)]
+}
+
+async function parseFile(raw: string, filepath: string): Promise<unknown> {
+  let result: unknown | undefined
+
+  // TODO: sniff format from raw text
+  if (filepath.endsWith(".json")) {
     try {
       result = JSON.parse(raw)
     } catch (err: any) {
       logger.error("error parsing json", err.stack)
-      throw new Error(`failed to parse json from file '${file}'`)
+      throw new Error(`failed to parse json from '${filepath}'`)
     }
   }
 
-  if (file.endsWith(".yaml") || file.endsWith(".yml")) {
+  if (filepath.endsWith(".yaml") || filepath.endsWith(".yml")) {
     try {
       result = yaml.load(raw)
     } catch (err: any) {
       logger.error("error parsing yaml", err.stack)
-      throw new Error(`failed to parse yaml from file '${file}'`)
+      throw new Error(`failed to parse yaml from '${filepath}'`)
     }
   }
 
   if (!result) {
-    throw new Error(`failed to load file '${file}'`)
+    throw new Error(`failed to parse '${filepath}'`)
   }
 
-  return [file, result]
-}
-
-async function loadRemoteFile(uri: string): Promise<[string, any]> {
-  // todo: https://github.com/mnahkies/openapi-code-generator/issues/43
-  throw new Error(`could not load ${uri} - not implemented,`)
+  return result
 }
 
 export function isRemote(location: string): boolean {
