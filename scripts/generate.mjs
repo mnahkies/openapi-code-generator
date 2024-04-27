9a2dcde1bcac97b6b002081b88eaa60f7cced2cc
Michael Nahkies <michael@nahkies.co.nz>
2024-04-27 16:52:17 +0100

    fix: deps needed for azure specs

--- scripts/generate.mjs
+++ scripts/generate.mjs
@@ -1,15 +1,29 @@
 import {exec, execSync} from "node:child_process"
 import path from "node:path"
 
-const templates = execSync(`find ./integration-tests -mindepth 1 -maxdepth 1 -type d`).toString("utf-8").split("\n").map(it => it.trim()).filter(Boolean)
-const definitions = execSync(`find ./integration-tests-definitions -type f`).toString("utf-8").split("\n").map(it => it.trim()).filter(Boolean)
+const templates = execSync(
+  `find ./integration-tests -mindepth 1 -maxdepth 1 -type d`,
+)
+  .toString("utf-8")
+  .split("\n")
+  .map((it) => it.trim())
+  .filter(Boolean)
+const definitions = execSync(`find ./integration-tests-definitions -type f`)
+  .toString("utf-8")
+  .split("\n")
+  .map((it) => it.trim())
+  .filter(Boolean)
 
-Promise.all(templates.flatMap(template => definitions.map(definition => runSingle(template, definition))))
+Promise.all(
+  templates.flatMap((template) =>
+    definitions.map((definition) => runSingle(template, definition)),
+  ),
+)
   .then(() => {
     console.log("success!")
     process.exit(1)
   })
-  .catch(err => {
+  .catch((err) => {
     console.error(err)
     process.exit(1)
   })
@@ -24,25 +38,27 @@ async function runSingle(template, input) {
     `--input-type=${inputType}`,
     `--output="integration-tests/${template}/src/generated/${filename}"`,
     `--template="${template}"`,
-    "--schema-builder=zod"
+    "--schema-builder=zod",
   ]
 
   try {
-    const result = await runCmd(`node ./packages/openapi-code-generator/dist/index.js ${args.join(" ")}`)
-    result.forEach(it => {
-      console.info(it)
+    const result = await runCmd(
+      `node ./packages/openapi-code-generator/dist/index.js ${args.join(" ")}`,
+    )
+    result.forEach((it) => {
+      console.info(`[${template} - ${filename}] ${it}`)
     })
   } catch (err) {
     console.error(err)
-    err.output.forEach(it => {
-      console.error(it)
+    err.output.forEach((it) => {
+      console.error(`[${template} - ${filename}] ${it}`)
     })
     throw err
   }
 }
 
 function runCmd(cmd) {
-  return new Promise(function(resolve, reject) {
+  return new Promise(function (resolve, reject) {
     const output = []
 
     console.info(cmd)
@@ -57,10 +73,10 @@ function runCmd(cmd) {
       resolve(output)
     })
 
-    child.stdout.on("data", it => {
+    child.stdout.on("data", (it) => {
       output.push(it.trim())
     })
-    child.stderr.on("data", it => {
+    child.stderr.on("data", (it) => {
       output.push(it.trim())
     })
   })
