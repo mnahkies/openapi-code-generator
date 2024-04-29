f935bb5a1e07c52dc1fee81ca5f53cace9f94362
Michael Nahkies <michael@nahkies.co.nz>
2024-04-29 19:54:58 +0100

    feat: publish docs from ci, add ga to docs (#163)

--- packages/documentation/src/pages/_document.tsx
+++ packages/documentation/src/pages/_document.tsx
@@ -1,9 +1,42 @@
 import {Head, Html, Main, NextScript} from "next/document"
+import Script from "next/script"
+
+const GA_ID = "G-X7JHJYGVNR"
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* https://developers.google.com/tag-platform/security/guides/consent?consentmode=advanced */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('consent', 'default', {
+            'ad_storage': 'denied',
+            'ad_user_data': 'denied',
+            'ad_personalization': 'denied',
+            'analytics_storage': 'denied',
+            'personalization_storage': 'denied',
+          });
+
+          gtag('js', new Date());
+
+          gtag('config', '${GA_ID}', {
+            page_path: window.location.pathname,
+            allow_google_signals: false,
+            allow_ad_personalization_signals: false,
+            restricted_data_processing: true
+          });
+        `,
+          }}
+        />
+      </Head>
+      <Script
+        strategy={"afterInteractive"}
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+      />
       <body>
         <Main />
         <NextScript />
