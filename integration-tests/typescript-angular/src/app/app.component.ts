a0bfe5716a11849dc74e7cf8d5a6a4d2fbaf28a0
Michael Nahkies <michael@nahkies.co.nz>
2024-05-25 10:00:43 +0100

    fix: add missing imports

--- integration-tests/typescript-angular/src/app/app.component.ts
+++ integration-tests/typescript-angular/src/app/app.component.ts
@@ -1,7 +1,11 @@
 import {Component} from '@angular/core'
 import {CommonModule} from '@angular/common'
 import {RouterOutlet} from '@angular/router'
+import {ApiModule as AzureCoreDataPlanServiceModule} from '../generated/azure-core-data-plane-service.tsp/api.module'
+import {ApiModule as AzureResourceManagerModule} from '../generated/azure-resource-manager.tsp/api.module'
 import {ApiModule as GhApiModule} from '../generated/api.github.com.yaml/api.module'
+import {ApiModule as OktaIdpModule} from '../generated/okta.idp.yaml/api.module'
+import {ApiModule as OktaOauthModule} from '../generated/okta.oauth.yaml/api.module'
 import {ApiModule as PetStoreApiModule} from '../generated/petstore-expanded.yaml/api.module'
 import {ApiModule as StripeApiModule} from '../generated/stripe.yaml/api.module'
 import {ApiModule as TodoListsApiModule} from '../generated/todo-lists.yaml/api.module'
@@ -12,7 +16,11 @@ import {ApiModule as TodoListsApiModule} from '../generated/todo-lists.yaml/api.
   imports: [
     CommonModule,
     RouterOutlet,
+    AzureCoreDataPlanServiceModule,
+    AzureResourceManagerModule,
     GhApiModule,
+    OktaIdpModule,
+    OktaOauthModule,
     PetStoreApiModule,
     StripeApiModule,
     TodoListsApiModule,
