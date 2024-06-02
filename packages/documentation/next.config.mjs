2c6f8c027fffb8340a482abd1bf77ed4f9e1c6d2
Michael Nahkies <michael@nahkies.co.nz>
2024-06-02 14:29:29 +0100

    feat: add an interactive playground to the documentation site

--- packages/documentation/next.config.mjs
+++ packages/documentation/next.config.mjs
@@ -1,4 +1,5 @@
 import nextra from "nextra"
+import NodePolyfillPlugin from "node-polyfill-webpack-plugin"
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -7,6 +8,20 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true,
+  },
+  webpack: (config, {buildId, dev, isServer, defaultLoaders, webpack}) => {
+    config.experiments = {...config.experiments, syncWebAssembly: true}
+    config.plugins.push(new NodePolyfillPlugin())
+    config.module.rules.push({
+      test: /node:.+/i,
+      use: "null-loader",
+    })
+    config.module.rules.push({
+      test: /@biomejs\/wasm-nodejs/i,
+      use: "null-loader",
+    })
+
+    return config
   },
   async redirects() {
     return [{source: "/", destination: "/overview/about", permanent: false}]
