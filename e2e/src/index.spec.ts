a6741c1aa4dd117970067fb9b405f532ea890c8d
Michael Nahkies <michael@nahkies.co.nz>
2024-08-17 11:12:47 +0100

    feat: add e2e test suite

---
+++ e2e/src/index.spec.ts
@@ -0,0 +1,40 @@
+import type {Server} from "node:http"
+import {beforeAll, describe, expect} from "@jest/globals"
+import {ApiClient} from "./generated/client/client"
+import {main} from "./index"
+
+describe("e2e", () => {
+  let server: Server
+  let client: ApiClient
+
+  beforeAll(async () => {
+    const args = await main()
+    client = new ApiClient({
+      basePath: `http://localhost:${args.address.port}`,
+      defaultHeaders: {},
+    })
+    server = args.server
+  })
+
+  afterAll(async () => {
+    server.close()
+  })
+
+  describe("GET /validation/numbers/random-number", () => {
+    it("returns a random number", async () => {
+      const res = await client.getValidationNumbersRandomNumber()
+      expect(res.status).toBe(200)
+
+      const body = await res.json()
+
+      expect(body).toEqual({
+        result: expect.any(Number),
+        params: {
+          min: 0,
+          max: 10,
+          forbidden: [],
+        },
+      })
+    })
+  })
+})
