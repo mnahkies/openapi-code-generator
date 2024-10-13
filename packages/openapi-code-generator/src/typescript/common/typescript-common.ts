e1f859fced73d591c8b871370d07ec2c495e6cb7
Michael Nahkies <michael@nahkies.co.nz>
2024-10-13 14:33:10 +0100

    feat: support emitting Implementation as interface

--- packages/openapi-code-generator/src/typescript/common/typescript-common.ts
+++ packages/openapi-code-generator/src/typescript/common/typescript-common.ts
@@ -118,11 +118,21 @@ export function asyncMethod({
 }
 
 export type ExportDefinition =
-  | {name: string; type?: string; value: string; kind: "const"}
   | {
+      kind: "const"
+      name: string
+      type?: string
+      value: string
+    }
+  | {
+      kind: "type"
       name: string
       value: string
-      kind: "type"
+    }
+  | {
+      kind: "interface"
+      name: string
+      value: string
     }
 
 export function buildExport(args: ExportDefinition) {
@@ -137,6 +147,8 @@ export function buildExport(args: ExportDefinition) {
       }`
     case "type":
       return `export type ${args.name} = ${args.value}`
+    case "interface":
+      return `export interface ${args.name} ${args.value}`
   }
 }
 
