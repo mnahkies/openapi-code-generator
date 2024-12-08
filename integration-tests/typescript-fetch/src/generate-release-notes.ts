7177a14cfd08540489ea8030db5b2d13c96f2cf5
Michael Nahkies <michael@nahkies.co.nz>
2024-12-08 12:12:13 +0000

    docs: add roadmap / release notes (#277)
    takes a stab at forming a roadmap, with a view to what's outstanding
    before the first v1 release,
    and what's near, mid or long term.

---
+++ integration-tests/typescript-fetch/src/generate-release-notes.ts
@@ -0,0 +1,76 @@
+import * as fs from "node:fs"
+import path from "node:path"
+import {
+  GitHubV3RestApi,
+  GitHubV3RestApiServers,
+} from "./generated/api.github.com.yaml/client"
+
+function formatDate(dateStr: string | null) {
+  if (!dateStr) return ""
+  return new Date(dateStr).toLocaleDateString("en-GB", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  })
+}
+
+export async function generateReleaseNotes() {
+  const client = new GitHubV3RestApi({
+    basePath: GitHubV3RestApiServers.default(),
+    defaultHeaders: {},
+  })
+
+  const res = await client.reposListReleases({
+    owner: "mnahkies",
+    repo: "openapi-code-generator",
+    perPage: 100,
+  })
+
+  if (res.status === 200) {
+    const body = await res.json()
+
+    return body.map(
+      (it) => `### ${formatDate(it.published_at)} (${it.name})
+${it.body
+  ?.split("\n")
+  .filter((it) => it.startsWith("#") === false)
+  .join("\n")}
+    `,
+    )
+  }
+
+  throw new Error("failed to fetch releases", {
+    cause: new Error(await res.text()),
+  })
+}
+
+async function main() {
+  const notes = await generateReleaseNotes()
+  const result = `import { Callout, Steps } from 'nextra/components'
+
+# Release notes
+
+<Callout emoji="💡">
+  This page is statically generated from the [Github releases](https://github.com/mnahkies/openapi-code-generator/releases),
+  and may sometimes be slightly out of date.
+</Callout>
+
+<Steps>
+${notes.join("\n")}
+</Steps>
+`
+
+  fs.writeFileSync(
+    path.resolve(
+      __dirname,
+      "../../../packages/documentation/src/pages/reference/release-notes.mdx",
+    ),
+    result.replace(/\r\n/g, "\n"),
+    "utf-8",
+  )
+}
+
+main().catch((err) => {
+  console.error(err)
+  process.exit(1)
+})
