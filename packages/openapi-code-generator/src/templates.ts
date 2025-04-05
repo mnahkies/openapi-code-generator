642f9e4d03f9a17a20bff0d05ae917bb417774ba
Michael Nahkies <michael@nahkies.co.nz>
2025-04-05 12:10:22 +0100

    feat: add metadata to generators

--- packages/openapi-code-generator/src/templates.ts
+++ packages/openapi-code-generator/src/templates.ts
@@ -1,11 +1,28 @@
+import type {Generator} from "./templates.types"
 import {generateTypescriptAngular} from "./typescript/typescript-angular/typescript-angular.generator"
 import {generateTypescriptAxios} from "./typescript/typescript-axios/typescript-axios.generator"
 import {generateTypescriptFetch} from "./typescript/typescript-fetch/typescript-fetch.generator"
 import {generateTypescriptKoa} from "./typescript/typescript-koa/typescript-koa.generator"
 
 export const templates = {
-  "typescript-fetch": generateTypescriptFetch,
-  "typescript-axios": generateTypescriptAxios,
-  "typescript-angular": generateTypescriptAngular,
-  "typescript-koa": generateTypescriptKoa,
-}
+  "typescript-fetch": {
+    language: "typescript",
+    type: "client",
+    run: generateTypescriptFetch,
+  },
+  "typescript-axios": {
+    language: "typescript",
+    type: "client",
+    run: generateTypescriptAxios,
+  },
+  "typescript-angular": {
+    language: "typescript",
+    type: "client",
+    run: generateTypescriptAngular,
+  },
+  "typescript-koa": {
+    language: "typescript",
+    type: "server",
+    run: generateTypescriptKoa,
+  },
+} satisfies {[key: string]: Generator}
