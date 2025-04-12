199c141f51a0e2b5f0952100271753922d93438b
Michael Nahkies <michael@nahkies.co.nz>
2025-04-12 11:41:58 +0100

    chore: regenerate

--- integration-tests/typescript-koa/src/generated/okta.oauth.yaml/generated.ts
+++ integration-tests/typescript-koa/src/generated/okta.oauth.yaml/generated.ts
@@ -117,13 +117,13 @@ import {
 import {
   KoaRuntimeResponder,
   KoaRuntimeResponse,
+  Params,
   Response,
   ServerConfig,
   StatusCode,
   startServer,
 } from "@nahkies/typescript-koa-runtime/server"
 import {
-  Params,
   parseRequestInput,
   responseValidationFactory,
 } from "@nahkies/typescript-koa-runtime/zod"
