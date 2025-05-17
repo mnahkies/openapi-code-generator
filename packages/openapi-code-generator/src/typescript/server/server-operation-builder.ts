c1245815548c24f6c33b21be97a2dc1c901fa0d3
Michael Nahkies <michael@nahkies.co.nz>
2025-05-17 09:35:31 +0100

    fix: smarter params, nextjs 15 support

--- packages/openapi-code-generator/src/typescript/server/server-operation-builder.ts
+++ packages/openapi-code-generator/src/typescript/server/server-operation-builder.ts
@@ -58,6 +58,7 @@ export type ServerOperationResponseSchemas = {
 }
 
 export type Parameters = {
+  hasParams: boolean
   type: string
   path: {
     schema: string | undefined
@@ -130,7 +131,16 @@ export class ServerOperationBuilder {
       ${header.type}
     >`
 
-    return {type, path, query, header, body}
+    return {
+      hasParams: Boolean(
+        path.schema || query.schema || header.schema || body.schema,
+      ),
+      type,
+      path,
+      query,
+      header,
+      body,
+    }
   }
 
   responseValidator(): string {
