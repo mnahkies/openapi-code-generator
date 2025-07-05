45a9490f5e7231c14fe0576affe331df4026faf9
Michael Nahkies <michael@nahkies.co.nz>
2025-07-05 15:12:01 +0100

    feat: wip documentation

--- integration-tests/typescript-nextjs/src/app/todo-lists.yaml/list/[listId]/route.ts
+++ integration-tests/typescript-nextjs/src/app/todo-lists.yaml/list/[listId]/route.ts
@@ -4,15 +4,15 @@ import {
   _PUT,
 } from "../../../../generated/todo-lists.yaml/list/[listId]/route"
 
-export const GET = _GET(async ({params}, respond, context) => {
+export const GET = _GET(async ({params}, respond, request) => {
   // TODO: implementation
   return respond.withStatus(501).body({message: "not implemented"} as any)
 })
-export const PUT = _PUT(async ({params, body}, respond, context) => {
+export const PUT = _PUT(async ({params, body}, respond, request) => {
   // TODO: implementation
   return respond.withStatus(501).body({message: "not implemented"} as any)
 })
-export const DELETE = _DELETE(async ({params}, respond, context) => {
+export const DELETE = _DELETE(async ({params}, respond, request) => {
   // TODO: implementation
   return respond.withStatus(501).body({message: "not implemented"} as any)
 })
