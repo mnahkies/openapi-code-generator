197b3761df705d350b61797bb3aacf145d0ec4fa
Michael Nahkies <michael@nahkies.co.nz>
2025-07-05 14:15:14 +0100

    feat: nextjs runtime package

---
+++ packages/typescript-nextjs-runtime/jest.config.js
@@ -0,0 +1,12 @@
+const base = require("../../jest.base")
+const {name: displayName} = require("./package.json")
+
+/**
+ * @type { import('@jest/types').Config.ProjectConfig }
+ */
+const config = {
+  ...base,
+  displayName,
+}
+
+module.exports = config
