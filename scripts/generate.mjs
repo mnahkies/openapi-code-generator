032e5c202093d575fea1a265c577b23befdd1109
Michael Nahkies <michael@nahkies.co.nz>
2025-07-24 13:30:02 +0100

    chore: generate.mjs supports filtering by template/spec (#353)
    QoL change to make it easier to regenerate integration tests for
    individual templates and/or specs

--- scripts/generate.mjs
+++ scripts/generate.mjs
@@ -1,5 +1,13 @@
+#!/usr/bin/env node
+
 import {exec, execSync} from "node:child_process"
 import path from "node:path"
+import {Command, Option} from "@commander-js/extra-typings"
+
+const program = new Command()
+  .addOption(new Option("-t --template <value>", "filter to a single template"))
+  .addOption(new Option("-s --spec <value>", "filter to a single spec"))
+  .showHelpAfterError()
 
 const templates = execSync(
   "find ./integration-tests -mindepth 1 -maxdepth 1 -type d",
@@ -8,16 +16,29 @@ const templates = execSync(
   .split("\n")
   .map((it) => it.trim())
   .filter(Boolean)
+
 const definitions = execSync("find ./integration-tests-definitions -type f")
   .toString("utf-8")
   .split("\n")
   .map((it) => it.trim())
   .filter(Boolean)
 
+const config = program.parse().opts()
+
+const filteredTemplate = config.template
+const filteredSpec = config.spec ? path.normalize(config.spec) : undefined
+
+console.info("filters", {filteredTemplate, filteredSpec})
 Promise.all(
-  templates.flatMap((templatePath) =>
-    definitions.map((definition) => runSingle(templatePath, definition)),
-  ),
+  templates
+    .filter((it) => !filteredTemplate || it.includes(filteredTemplate))
+    .flatMap((templatePath) =>
+      definitions
+        .filter(
+          (it) => !filteredSpec || path.normalize(it).includes(filteredSpec),
+        )
+        .map((definition) => runSingle(templatePath, definition)),
+    ),
 )
   .then(() => {
     console.log("success!")
