0c60a5daf6031eeb091914b11a92a3d3c132e05e
Michael Nahkies <michael@nahkies.co.nz>
2025-10-26 11:59:58 +0000

    fix: lint

--- scripts/generate-toc.mjs
+++ scripts/generate-toc.mjs
@@ -1,16 +1,16 @@
-import {remark} from 'remark'
-import remarkToc from 'remark-toc'
-import {readFile, writeFile} from "node:fs/promises";
+import {readFile, writeFile} from "node:fs/promises"
+import {remark} from "remark"
+import remarkToc from "remark-toc"
 
 async function generateToc(filename) {
   const result = await remark()
     .use(remarkToc)
-    .process(await readFile(filename, 'utf-8'))
+    .process(await readFile(filename, "utf-8"))
 
-  await writeFile(filename, String(result), 'utf-8')
+  await writeFile(filename, String(result), "utf-8")
 }
 
-await generateToc('./README.md')
-await generateToc('./CONTRIBUTING.md')
-await generateToc('./packages/openapi-code-generator/README.md')
-await generateToc('./packages/documentation/README.md')
+await generateToc("./README.md")
+await generateToc("./CONTRIBUTING.md")
+await generateToc("./packages/openapi-code-generator/README.md")
+await generateToc("./packages/documentation/README.md")
