ae5d7b8a798801def7fdaadb1d39bb8a330c6e6c
Michael Nahkies <michael@nahkies.co.nz>
2025-10-26 11:56:02 +0000

    fix: replace markdown-toc with remark-toc
    `markdown-toc` appears somewhat abandoned, with a PR I raised to upgrade `remarkable` going unlooked at ~6 months.
    
    (ref: https://github.com/jonschlinkert/markdown-toc/pull/199)
    
    replace it with `remark` / `remark-toc` to clear CVE warnings.

--- scripts/generate-docs.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -eo pipefail
-
-pnpm markdown-toc -i --bullets=- ./README.md
-pnpm markdown-toc -i --bullets=- ./CONTRIBUTING.md
-pnpm markdown-toc -i --bullets=- ./packages/openapi-code-generator/README.md
-pnpm markdown-toc -i --bullets=- ./packages/documentation/README.md
