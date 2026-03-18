ede0d2593ec2dd78d6527965fcdc2d2fd14f8804
Michael Nahkies <michael@nahkies.co.nz>
2026-03-18 15:29:37 +1300

    chore: add pnpm run ci-pipeline:fast

--- scripts/ci-pipeline.sh
+++ scripts/ci-pipeline.sh
@@ -8,6 +8,10 @@ SCHEMA_BUILDERS=(
   zod-v4
 )
 
+if [[ -n "$FAST" ]]; then
+  SCHEMA_BUILDERS=(zod-v4)
+fi
+
 pnpm run build
 pnpm run build:docs
 pnpm ci-test
@@ -21,8 +25,10 @@ for SCHEMA_BUILDER in "${SCHEMA_BUILDERS[@]}"; do
   pnpm integration:validate
 done
 
-SCHEMA_BUILDER=zod-v3 pnpm e2e:generate
-pnpm e2e:validate
+if [[ -z "$FAST" ]]; then
+  SCHEMA_BUILDER=zod-v3 pnpm e2e:generate
+  pnpm e2e:validate
+fi
 
 SCHEMA_BUILDER=zod-v4 pnpm e2e:generate
 pnpm e2e:validate
