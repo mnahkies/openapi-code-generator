1a85fb342b5bbed956935ec394b2740717de7758
Michael Nahkies <michael@nahkies.co.nz>
2026-05-30 16:17:41 +0100

    Update vitest.base.ts

--- vitest.base.ts
+++ vitest.base.ts
@@ -4,7 +4,7 @@ import {defineProject} from "vitest/config"
 export const baseTestConfig = defineProject({
   test: {
     environment: "node",
-    include: ["**/*.spec.ts"],
+    include: ["**/*.spec.ts", "**/*.integration.ts"],
     globals: false,
     mockReset: true,
     testTimeout: inspector.url() ? 5 * 60 * 1000 : 5 * 1000,
